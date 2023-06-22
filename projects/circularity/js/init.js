cb01dd6f987b8e6d7eca21c69a8fb0d001cb052e
RoyWilkes40 Wilkes <roy.wilkes40@gmail.com>
2023-06-22 20:52:16 +0000

    refactor

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -22,21 +22,17 @@ var init = function (window) {
     // TODO 2 : Create a function that draws a circle
     function drawCircle() {
       circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-      physikz.addRandomVelocity(circle, canvas);
+      physikz.addRandomVelocity(circle, canvas, 10, 10);
       view.addChild(circle);
       circles.push(circle);
     }
 
     // TODO 3 / 7 : Call the drawCircle() function
-  //  var loopsCompleted = 100;
-  //  while (loopsCompleted < 100){
-    // drawCircle();
+
   //  }
+for(var i = 0; i < 100; i++ ){
   drawCircle();
-  drawCircle();
-  drawCircle();
-  drawCircle();
-  drawCircle();
+}
 
     ////////////////////////////////////////////////////////////
     ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -50,18 +46,13 @@ var init = function (window) {
     function update() {
       // TODO 4 : Update the circle's position //
       physikz.updatePosition(circles[0]);
-      physikz.updatePosition(circles[1]);
-      physikz.updatePosition(circles[2]);
-      physikz.updatePosition(circles[3]);
-      physikz.updatePosition(circles[4]);
-
+      for(var i =0; i < circles.length; i++){
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
+     
       // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-      game.checkCirclePosition(circles[0]);
-      game.checkCirclePosition(circles[1]);
-      game.checkCirclePosition(circles[2]);
-      game.checkCirclePosition(circles[3]);
-      game.checkCirclePosition(circles[4]);
-
+     
       // TODO 9 : Iterate over the array
     }
 
